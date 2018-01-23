89e13f7b061667637aa52b990f5b937357b63680
Baz Jay <jaybaz1@gmail.com>
2018-01-23 22:41:54 +0000

    mongo fetch

---
+++ playground/mongodb-find.js
@@ -0,0 +1,40 @@
+
+//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err){
+        return console.log('Unable to connect to mongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({completed: false}).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5a67b613deeb2775d4400045')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // })
+
+    db.collection('Todos').find().count().then((count) => {
+        console.log(`Todos count: ${count}`);
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });
+
+    db.collection('Users').find({name: 'Baz Jay'}).toArray().then((docs) => {
+        console.log('Users');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch users', err);
+    });
+    //db.close();
+});
